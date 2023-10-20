d0171ade35071386461f3dff981d54d58b64b8bc
Konstantin <k.moskal9901@gmail.com>
2023-10-20 13:44:45 +0300

    clear logs 0.2.2s

--- src/5arcanoid.js
+++ src/5arcanoid.js
@@ -14,7 +14,7 @@ var gameOffsetY = 10; // Смещение игровой области по о�
 
 var cellWidth = 146;
 var cellHeight = 67;
-var game_version = "v 0.2.1s";
+var game_version = "v 0.2.2s";
 var zero ={
     x: 460,
     y: 125
